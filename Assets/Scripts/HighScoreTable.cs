6158b771fc33879de53fdc099c1bc7dfc7fddc0c
Damien <hadesrealm@gmail.com>
2015-08-22 21:44:48 -0700

    Finished implementing the EndScene

--- Assets/Scripts/HighScoreTable.cs
+++ Assets/Scripts/HighScoreTable.cs
@@ -28,7 +28,7 @@ namespace GSP
         Transform body;                 // The panel at HighScoresTable/Body
 
         // Creates a HighScoreTable object to deal with the scores
-        public HighScoreTable(bool isSaveLoad = false)
+        public HighScoreTable()
         {
             // Initialise the list
             scores = new List<Pair<string, int>>();
@@ -39,37 +39,35 @@ namespace GSP
             // Set the max number of entries
             maxScores = 10;
 
-            // Make sure we're not saving or loading
-            if (!isSaveLoad)
-            {
-                // Load the highscores from GameMaster
-                GameMaster.Instance.LoadHighScores();
-
-                // Check if there aren't ten entries in the list
-                if (scores.Count < maxScores)
-                {
-                    // Initialise the table to empty scores
-                    for (int index = 0; index < maxScores; index++)
-                    {
-                        // Using this function so we don't save ten times
-                        AddScoreFromSave("Empty", 0);
-                    } // end for
-
-                    // Now sort the scores
-                    SortScores();
-
-                    // Finally, save the scores
-                    GameMaster.Instance.SaveHighScores();
-                } // end if
-            } // end if
-
             // Set the table's interface colour to the player's colour
             GameObject.Find("Canvas").transform.Find("HighScoresTable").GetComponent<Image>().color =
                 Utility.InterfaceColorToColor(GameMaster.Instance.GetPlayerColor(0));
 
-            // Now display the scores
-            DisplayScores();
+            // Set the Back to Menu button's panel to the player's colour
+            GameObject.Find("Canvas").transform.Find("ButtonPanel").GetComponent<Image>().color =
+                Utility.InterfaceColorToColor(GameMaster.Instance.GetPlayerColor(0));
         } // end HighScoreTable
+
+        // Fills the table with empty entries if there are less than the max
+        public void FillTable()
+        {
+            // Check if there aren't ten entries in the list
+            if (scores.Count < maxScores)
+            {
+                // Initialise the table to empty scores
+                for (int index = 0; index < maxScores; index++)
+                {
+                    // Using this function so we don't save ten times
+                    AddScoreFromSave("Empty", 0);
+                } // end for
+
+                // Now sort the scores
+                SortScores();
+
+                // Finally, save the scores
+                GameMaster.Instance.SaveHighScores();
+            } // end if
+        } // end FillTable
 
         // Gets a score from the list
         public Pair<string, int> GetScore(int index)
@@ -119,15 +117,18 @@ namespace GSP
         // Sets the scores for display on the table.
         void DisplayScores()
         {
-            Debug.LogFormat("maxScores: {0}", maxScores);
-            Debug.LogFormat("scores.Count: {0}", scores.Count);
-            
             // Loop over the table to set the values
             for (int index = 0; index < maxScores; index++)
             {
+                // Make sure the index is less than the scores count
+                if (index >= scores.Count)
+                {
+                    // Break out of the loop
+                    break;
+                } // end if
+
                 // Get the child at index + 1
                 Transform entry = body.GetChild(index + 1);
-                Debug.LogFormat("Name: {0}", entry.name);
 
                 // Get the children of the entry and their text components and set their text to the entry values
                 entry.GetChild(0).GetComponent<Text>().text = scores[index].First;
