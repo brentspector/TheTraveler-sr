d7341625f356997fba7b178f13009a9481470309
brentspector <brent.spector@yahoo.com>
2015-09-09 13:53:55 -0700

    Set Up AI Folder
    Not an important update.

---
+++ Assets/Scripts/Char/Ai/AI.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+namespace GSP.Char.AI
+{
+
+	public class AI : MonoBehaviour 
+	{
+
+		// Use this for initialization
+		void Start () {
+	
+		}
+	
+		// Update is called once per frame
+		void Update () {
+	
+		}
+	} // end class AI
+} // end namespace GSP.Char.AI
